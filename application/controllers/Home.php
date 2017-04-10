032fe0337bbfcfc2b794d23a21a362847823a189
yangchunyang <chunyang.yang@innocellence.com>
2017-04-10 10:59:36 +0800

    修改主页模型

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -13,11 +13,12 @@ class Home extends CI_Controller {
 
 	public function index()
 	{
+
 		$this->load->helper('url');
 		$this->load->vars('resource',base_url('resource/'));
-		$this->hom->artlist();
 		$this->load->view('common/header');
 	    $this->load->vars('resource',base_url('resource/'));
+
 		$this->load->view('home');
 		$this->load->view('common/footer');
 	}
