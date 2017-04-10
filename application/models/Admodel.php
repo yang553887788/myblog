4aefdaa84eca3583548147756a715a6d4d9da481
yangchunyang <chunyang.yang@innocellence.com>
2017-04-10 14:18:34 +0800

    模型

--- application/models/Admodel.php
+++ application/models/Admodel.php
@@ -5,8 +5,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 class Admodel extends CI_Model
 {
-	public function __construct(){
+	public function __construct()
+	{
 		parent::__construct();
 		$this->load->database();
+	}
+
+	public function adlist(){
+		$res = $this->load->get('my_admin');
+		$list = $res->result_array();
+		return $list;
 	}
 }
